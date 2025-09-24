0625e1eb8213fb829e15883fde368d31a734c97a
Manish Sharma <111164468+Manish3515@users.noreply.github.com>
2025-09-24 11:24:37 +0000

    Add conversion operation examples

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,76 @@
-let score = "Manish"
+// let score = "Manish"
 
-// console.log(typeof score);
-// console.log(typeof (score));
+// // console.log(typeof score);
+// // console.log(typeof (score));
 
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1 ; false => 0 
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1 ; false => 0 
 
-let isLoggedIn = "Manish"
+// let isLoggedIn = "Manish" 
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false; 
-// "Manish" => true 
+// // 1 => true; 0 => false
+// // "" => false; 
+// // "Manish" => true 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************** Operations *************//
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Manish"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log(1+ 2 + "2");
+
+// console.log(3 + 5 * 6 % 7);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+ 
+
+ 
+
+
 
 
